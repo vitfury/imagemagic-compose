1f69701789cafea9b3f0cb13f76f757b90d364b0
Omelchenko <webmaster@email.ua>
2020-10-14 13:51:21 +0300

    fix certbot

--- compose/certbot/run-certbot.sh
+++ compose/certbot/run-certbot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+mkdir -m 744 /var/www/letsencrypt
 letsencrypt certonly --webroot -w /var/www/letsencrypt -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
 
 cp /etc/letsencrypt/archive/"$CN"/cert1.pem /var/certs/cert1.pem
 cp /etc/letsencrypt/archive/"$CN"/privkey1.pem /var/certs/privkey1.pem
+
+#letsencrypt certonly --webroot -w /var/www/letsencrypt -d "sticker.pub" --agree-tos --email "omelchenkovitaly@gmail.com" --non-interactive --text --dry-run
