08958453a8a32a868e40b3d97ba4ec5a21d88aaf
Omelchenko <webmaster@email.ua>
2020-12-02 19:20:23 +0200

    Certificate + nginx conf + volume storage

--- compose/certbot/run-certbot.sh
+++ compose/certbot/run-certbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-letsencrypt certonly --webroot -w /var/www/public -d "$CN" --agree-tos --email "$EMAIL" --non-interactive --text
+letsencrypt certonly --webroot -w /var/www/public -d "$CN" -d "$WWWCN" --agree-tos --email "$EMAIL" --non-interactive --text
 
 cp /etc/letsencrypt/live/sticker.pub/cert.pem /var/certs/cert.pem
 cp /etc/letsencrypt/live/sticker.pub/chain.pem /var/certs/chain.pem
 cp /etc/letsencrypt/live/sticker.pub/fullchain.pem /var/certs/fullchain.pem
 cp /etc/letsencrypt/live/sticker.pub/privkey.pem /var/certs/privkey.pem
 
-#letsencrypt certonly --webroot -w /var/www/public -d "sticker.pub" --agree-tos --email "omelchenkovitaly@gmail.com" --non-interactive --text --dry-run
+#letsencrypt certonly --webroot -w /var/www/public -d "sticker.pub" -d "www.sticker.pub" --agree-tos --email "omelchenkovitaly@gmail.com" --non-interactive --text --dry-run
